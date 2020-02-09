1384da9b94c5656038f2027cc628a32cdbfdccee
Tyler <tylerthomasallen@gmail.com>
2020-02-08 17:31:12 -0800

    add app router

---
+++ components/AppRouter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// Components
+import { Text} from 'react-native'
+
+
+class AppRouter extends Component {
+  render() {
+    return <Text>Test</Text>
+  }
+}
+
+export default AppRouter;
